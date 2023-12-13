226ac9daa566956a25c67c713fbad7aa6d84995b
Baktyiarov Aitegin <baktyiarovaitegin@gmail.com>
2023-12-13 15:32:14 +0600

    newWall

--- IHateWalls/Assets/Scripts/Wall/WallPiece.cs
+++ IHateWalls/Assets/Scripts/Wall/WallPiece.cs
@@ -3,15 +3,28 @@ using UnityEngine;
 
 namespace Walls
 {
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(MeshCollider))]
     public class WallPiece : MonoBehaviour, IDamagable
     {
         public bool isRealeased { get; private set; }
 
         [SerializeField] private Rigidbody _rigidbody;
 
-        private void Start()
+        private void OnValidate()
         {
-            _rigidbody = GetComponent<Rigidbody>();
+            if (Application.isPlaying == true) return;
+
+            if (_rigidbody == null)
+            {
+                _rigidbody = GetComponent<Rigidbody>();
+            }
+
+            _rigidbody.useGravity = false;
+            _rigidbody.isKinematic = true;
+            _rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+            GetComponent<MeshCollider>().convex = true;
         }
 
         public void Damage(float damage)
